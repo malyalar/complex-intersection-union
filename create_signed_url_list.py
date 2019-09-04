89af093708b7577a9bf5004971491d93607cc416
Rohit Malyala <38365101+malyalar@users.noreply.github.com>
2019-09-04 11:36:49 -0700

    Update and rename create_csv_imageURLs.py to create_signed_url_list.py

--- create_signed_url_list.py
+++ create_signed_url_list.py
@@ -19,53 +19,46 @@ aws_access_key_id = 'XXXXXXXXXXXXXXXXXX'
 aws_secret_access_key = 'XXXXXXXXXXXXXXXXXXXXX+XXXX'
 """
 
-# create client object    
-client = boto3.client(
-    's3',
-    region_name=region,
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key
-    )
+session = boto3.session.Session(region_name=region)
+s3client = session.client('s3', config= boto3.session.Config(signature_version='s3v4'))
 
-# Interesting conda CLI argument: aws s3 ls s3://<name_of_bucket>/<key_prefix>/ --recursive
 
 # List all the buckets with the account
-bucket_list = client.list_buckets()
+bucket_list = s3.list_buckets()
 print('Existing Buckets: ')
 
 for bucket in bucket_list['Buckets']:
     print(f' {bucket["Name"]}')
 
-# create a raw list of the objects or contents of the bucket.
-object_list = client.list_objects(Bucket='lithotripsystonefinder')
 
-# get URL for the bucket of interest
-URL_header ='https://lithotripsystonefinder.s3.ca-central-1.amazonaws.com/'
-
-# parsing function to get 'key' just makes a list of only
-for key in object_list['Contents']:
-    print(URL_header + key['Key'])
+# list objects
+object_list = s3.list_objects(Bucket='lithotripsystonefinder')
 
 
-# arrange unordered dictionary into an array
-
-
-# create CSV with URLs 
-# 'image_url' is the name of the header
-    
 with open('input.csv', 'w', newline='') as csvFile:
-    writer = csv.writer(csvFile)
-   
-    for key in object_list['Contents']:
-        URL_full = URL_header + key['Key']
-        writer.writerow([URL_full])
 
+    writer = csv.writer(csvFile)
+
+    for key in object_list['Contents']:
+    
+        # s3client.get_object(Bucket='lithotripsystonefinder', Key=key['Key'])
+        presigned_url = s3client.generate_presigned_url('get_object',
+        Params={
+          'Bucket': "lithotripsystonefinder",
+          'Key': key['Key'],
+          },
+        ExpiresIn=604800,
+        )
+        writer.writerow([presigned_url])
+
+
+# replace the header [0] slot with "input_url"
 
 f = open('input.csv', 'r', newline = '')
 reader = csv.reader(f)
 mylist = list(reader)
 f.close()
-mylist[0] = ['input_url']
+mylist[0] = ['image_url']
 
 my_new_list = open('input.csv', 'w', newline = '')
 csv_writer = csv.writer(my_new_list)
